e86ea19d2cf52d43a93de4a52415a4ee9acf3b1e
SilviuHg <hagisilviu09@gmail.com>
2026-07-06 17:11:43 +0300

    setup frontend microservice

---
+++ frontend/app.js
@@ -0,0 +1,16 @@
+document.getElementById('fetch-btn').addEventListener('click', async () => {
+    try {
+        // k8s: acces BE on NodePort 30001
+        // for local testing: http://localhost:3000/data
+        const response = await fetch('http://localhost:30001/data');
+        const data = await response.json();
+
+        document.getElementById('msg').innerText = data.message;
+        document.getElementById('status').innerText = data.status;
+        document.getElementById('time').innerText = new Date(data.timestamp).toLocaleTimeString();
+        
+        document.getElementById('result-box').classList.remove('hidden');
+    } catch (error) {
+        alert('Eroare la conectarea cu API-ul: ' + error.message);
+    }
+});
